b0732adcd6d3360674f4d103316897034faff6d4
george <georgii.gabaidze@gmail.com>
2024-10-20 07:33:10 +0300

    updated screen and model

---
+++ lib/data/models/film_model.dart
@@ -0,0 +1,32 @@
+import 'package:film_zone/core/env.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'film_model.freezed.dart';
+
+part 'film_model.g.dart';
+
+@freezed
+class FilmModel with _$FilmModel {
+  const factory FilmModel({
+    required bool adult,
+    String? backdropPath,
+    required List<int>? genreIds,
+    required int id,
+    required String originalLanguage,
+    required String originalTitle,
+    required String overview,
+    required double popularity,
+    String? posterPath,
+    required String releaseDate,
+    required String title,
+    required bool video,
+    required double voteAverage,
+    required int voteCount,
+  }) = _FilmModel;
+
+  factory FilmModel.fromJson(Map<String, dynamic> json) => _$FilmModelFromJson(json);
+}
+
+extension FilmModelX on FilmModel {
+  String get posterUrl => posterPath != null ? '${Env.baseImagePath}$posterPath' : '';
+}
