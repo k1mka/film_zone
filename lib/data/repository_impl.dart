4dddbb309e41e5cabba6a637ac76f6bfaa7c044a
george <georgii.gabaidze@gmail.com>
2024-10-20 16:56:38 +0300

    updated searching

--- lib/data/repository_impl.dart
+++ lib/data/repository_impl.dart
@@ -10,27 +10,44 @@ class RepositoryImpl implements Repository {
   final NetworkService networkService;
 
   static const String _searchEndpoint = '/search/movie';
-
   static const _tokenTypeKey = 'Bearer';
   static const _authKey = 'Authorization';
-
   static const _queryKey = 'query';
   static const _resultsKey = 'results';
+  static const _pageKey = 'page';
 
   @override
-  Future<List<FilmModel>> searchFilms(String query) async {
+  Future<List<FilmModel>> searchFilms({
+    required String query,
+    required List<FilmModel> currentFilms,
+    required int currentPage,
+  }) async {
+    try {
+      final newFilms = await _getCatalog(query, currentPage);
+      final updatedFilms = [...currentFilms, ...newFilms];
+
+      return updatedFilms;
+    } catch (error) {
+      rethrow;
+    }
+  }
+
+  Future<List<FilmModel>> _getCatalog(String query, int currentPage) async {
     try {
       final result = await networkService.get(
         url: '${Env.baseUrl}$_searchEndpoint',
-        queryParameters: {_queryKey: query},
+        queryParameters: {
+          _queryKey: query,
+          _pageKey: currentPage,
+        },
         headers: {_authKey: '$_tokenTypeKey ${Env.apiKey}'},
       );
 
-      final response = result.data[_resultsKey];
+      final catalog = (result.data[_resultsKey] as List)
+          .map<FilmModel>((film) => FilmModel.fromJson(film as JsonMap))
+          .toList();
 
-      final films = response.map<FilmModel>((film) => FilmModel.fromJson(film as JsonMap)).toList();
-
-      return films;
+      return catalog;
     } catch (error) {
       rethrow;
     }
--- lib/domain/repository.dart
+++ lib/domain/repository.dart
@@ -1,5 +1,9 @@
 import 'package:film_zone/data/models/film_model.dart';
 
 abstract interface class Repository {
-  Future<List<FilmModel>> searchFilms(String query);
+  Future<List<FilmModel>> searchFilms({
+    required String query,
+    required List<FilmModel> currentFilms,
+    required int currentPage,
+  });
 }
